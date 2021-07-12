ba2bf2f166612defbf2b54cf71726084ec85f868
haoyu <haoyu.wang@geocomply.com>
2021-07-11 18:35:41 -0700

    Update makefile and ggave for V3.0

--- Sources/ggwave/resampler.cpp
+++ Sources/ggwave/resampler.cpp
@@ -1,7 +1,12 @@
 #include "resampler.h"
 
+#include <cassert>
 #include <cmath>
 #include <cstdio>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 namespace {
 double linear_interp(double first_number, double second_number, double fraction) {
@@ -9,71 +14,117 @@ double linear_interp(double first_number, double second_number, double fraction)
 }
 }
 
+Resampler::Resampler() :
+    m_sincTable(kWidth*kSamplesPerZeroCrossing),
+    m_delayBuffer(3*kWidth),
+    m_edgeSamples(kWidth),
+    m_samplesInp(2048) {
+    make_sinc();
+    reset();
+}
+
+void Resampler::reset() {
+    m_state = {};
+    std::fill(m_edgeSamples.begin(), m_edgeSamples.end(), 0.0f);
+    std::fill(m_delayBuffer.begin(), m_delayBuffer.end(), 0.0f);
+    std::fill(m_samplesInp.begin(), m_samplesInp.end(), 0.0f);
+}
+
 int Resampler::resample(
         float factor,
         int nSamples,
         const float * samplesInp,
         float * samplesOut) {
-    if (factor != m_lastFactor) {
-        make_sinc();
-        m_lastFactor = factor;
-    }
-
-    int idxInp = 0;
+    int idxInp = -1;
     int idxOut = 0;
     int notDone = 1;
-    double time_now = 0.0;
-    long num_samples = nSamples;
-    long int_time = 0;
-    long last_time = 0;
-    float data_in = samplesInp[idxInp];
-    float data_out;
+    float data_in = 0.0f;
+    float data_out = 0.0f;
     double one_over_factor = 1.0;
+
+    auto stateSave = m_state;
+
+    m_state.nSamplesTotal += nSamples;
+
+    if (samplesOut) {
+        assert(nSamples > kWidth);
+        if ((int) m_samplesInp.size() < nSamples + kWidth) {
+            m_samplesInp.resize(nSamples + kWidth);
+        }
+        for (int i = 0; i < kWidth; ++i) {
+            m_samplesInp[i] = m_edgeSamples[i];
+            m_edgeSamples[i] = samplesInp[nSamples - kWidth + i];
+        }
+        for (int i = 0; i < nSamples; ++i) {
+            m_samplesInp[i + kWidth] = samplesInp[i];
+        }
+        samplesInp = m_samplesInp.data();
+    }
+
     while (notDone) {
+        while (m_state.timeLast < m_state.timeInt) {
+            if (++idxInp >= nSamples) {
+                notDone = 0;
+                break;
+            } else {
+                data_in = samplesInp[idxInp];
+            }
+            //printf("xxxx idxInp = %d\n", idxInp);
+            if (samplesOut) new_data(data_in);
+            m_state.timeLast += 1;
+        }
+
+        if (notDone == false) break;
+
         double temp1 = 0.0;
-        long left_limit = time_now - kWidth + 1;      /* leftmost neighboring sample used for interp.*/
-        long right_limit = time_now + kWidth; /* rightmost leftmost neighboring sample used for interp.*/
-        if (left_limit<0) left_limit = 0;
-        if (right_limit>num_samples) right_limit = num_samples;
-        if (factor<1.0) {
-            for (int j=left_limit;j<right_limit;j++) {
-                temp1 += gimme_data(j-int_time)*sinc(time_now - (double) j);
+        int left_limit = m_state.timeNow - kWidth + 1; /* leftmost neighboring sample used for interp.*/
+        int right_limit = m_state.timeNow + kWidth;    /* rightmost leftmost neighboring sample used for interp.*/
+        if (left_limit < 0) left_limit = 0;
+        if (right_limit > m_state.nSamplesTotal + kWidth) right_limit = m_state.nSamplesTotal + kWidth;
+        if (factor < 1.0) {
+            for (int j = left_limit; j < right_limit; j++) {
+                temp1 += gimme_data(j - m_state.timeInt)*sinc(m_state.timeNow - (double) j);
             }
             data_out = temp1;
         }
         else {
             one_over_factor = 1.0 / factor;
-            for (int j=left_limit;j<right_limit;j++) {
-                temp1 += gimme_data(j-int_time)*one_over_factor*sinc(one_over_factor * (time_now - (double) j));
+            for (int j = left_limit; j < right_limit; j++) {
+                temp1 += gimme_data(j - m_state.timeInt)*one_over_factor*sinc(one_over_factor*(m_state.timeNow - (double) j));
             }
             data_out = temp1;
         }
 
-        //printf("%8.8f %8.8f\n", data_in, data_out);
-        samplesOut[idxOut++] = data_out;
-        time_now += factor;
-        last_time = int_time;
-        int_time = time_now;
-        while(last_time<int_time)      {
-            if (++idxInp == nSamples) {
+        if (samplesOut) {
+            //printf("inp = %d, l = %d, r = %d, n = %d, a = %d, b = %d\n", idxInp, left_limit, right_limit, m_state.nSamplesTotal, left_limit - m_state.timeInt, right_limit - m_state.timeInt - 1);
+            samplesOut[idxOut] = data_out;
+        }
+        ++idxOut;
+
+        m_state.timeNow += factor;
+        m_state.timeLast = m_state.timeInt;
+        m_state.timeInt = m_state.timeNow;
+        while (m_state.timeLast < m_state.timeInt) {
+            if (++idxInp >= nSamples) {
                 notDone = 0;
+                break;
             } else {
                 data_in = samplesInp[idxInp];
             }
-            new_data(data_in);
-            last_time += 1;
+            if (samplesOut) new_data(data_in);
+            m_state.timeLast += 1;
         }
-        //                if (!(int_time % 1000)) printf("Sample # %li\n",int_time);
-        //if (!(int_time % 1000)) {
-        //    printf(".");
-        //    fflush(stdout);
-        //}
+        //printf("last idxInp = %d, nSamples = %d\n", idxInp, nSamples);
+    }
+
+    if (samplesOut == nullptr) {
+        m_state = stateSave;
     }
 
     return idxOut;
 }
 
-float Resampler::gimme_data(long j) const {
+float Resampler::gimme_data(int j) const {
     return m_delayBuffer[(int) j + kWidth];
 }
 
@@ -88,7 +139,7 @@ void Resampler::make_sinc() {
     double temp, win_freq, win;
     win_freq = M_PI/kWidth/kSamplesPerZeroCrossing;
     m_sincTable[0] = 1.0;
-    for (int i = 1; i < kWidth*kSamplesPerZeroCrossing;i++) {
+    for (int i = 1; i < kWidth*kSamplesPerZeroCrossing; i++) {
         temp = (double) i*M_PI/kSamplesPerZeroCrossing;
         m_sincTable[i] = sin(temp)/temp;
         win = 0.5 + 0.5*cos(win_freq*i);
@@ -102,7 +153,7 @@ double Resampler::sinc(double x) const {
     if (fabs(x) >= kWidth - 1) {
         return 0.0;
     } else {
-        temp = fabs(x) * (double) kSamplesPerZeroCrossing;
+        temp = fabs(x)*(double) kSamplesPerZeroCrossing;
         low = temp;          /* these are interpolation steps */
         delta = temp - low;  /* and can be ommited if desired */
         return linear_interp(m_sincTable[low], m_sincTable[low + 1], delta);
